80c3699e9e5c2bba9ff72b4e0fe5b6d484921e52
Alexandre FAURÉ <alexandre.faure@edisys.eu>
2020-03-04 16:03:24 +0100

    Create model and export to JSON file

--- ExcelParser.cs
+++ ExcelParser.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 using NPOI.HSSF.UserModel;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
+using TestExcelParser.Model;
+using CellType = NPOI.SS.UserModel.CellType;
+using File = System.IO.File;
 
 namespace TestExcelParser
 {
@@ -30,10 +36,13 @@ namespace TestExcelParser
 
             var formatter = new DataFormatter();
 
+            var cellsList = new List<Cell>();
+
             var sheetsNumber = workbook.NumberOfSheets;
-            for (var s = 0; s < sheetsNumber; s++)
+            sheetsNumber = 1;
+            for (var sheetIndex = 0; sheetIndex < sheetsNumber; sheetIndex++)
             {
-                var sheet = workbook.GetSheetAt(s);
+                var sheet = workbook.GetSheetAt(sheetIndex);
                 if (sheet != null)
                 {
                     _treeRowsList = new List<TreeNode>();
@@ -51,62 +60,111 @@ namespace TestExcelParser
                             break;
                         }
 
+                        
                         foreach (var cell in curRow.Cells)
                         {
                             if (cell != null)
                             {
                                 // 1. AddCell
-                                var cellNodesList = new List<TreeNode>();
-
-                                cellNodesList.Add(new TreeNode(nameof(cell.RowIndex) + " = " + cell.RowIndex));
-                                cellNodesList.Add(new TreeNode(nameof(cell.ColumnIndex) + " = " + cell.ColumnIndex));
-
-                                cellNodesList.Add(
-                                    new TreeNode(nameof(cell.CellStyle.Alignment) + " = " + cell.CellStyle.Alignment));
-
-                                cellNodesList.Add(new TreeNode(nameof(cell.IsMergedCell) + " = " + cell.IsMergedCell));
 
                                 var cellFont = cell.CellStyle.GetFont(workbook);
-                                cellNodesList.Add(new TreeNode(nameof(cellFont.FontName) + " = " + cellFont.FontName));
-                                cellNodesList.Add(new TreeNode(nameof(cellFont.Color) + " = " + cellFont.Color));
-                                cellNodesList.Add(new TreeNode(nameof(cellFont.IsBold) + " = " + cellFont.IsBold));
-                                cellNodesList.Add(new TreeNode(nameof(cellFont.IsItalic) + " = " + cellFont.IsItalic));
-                                cellNodesList.Add(
-                                    new TreeNode(nameof(cellFont.FontHeight) + " = " + cellFont.FontHeight));
+
+                                var cellObject = new Cell
+                                {
+                                    column = cell.ColumnIndex,
+                                    row = cell.RowIndex,
+                                    sheet = sheetIndex,
+                                    style = new Style
+                                    {
+                                        alignment = cell.CellStyle.Alignment,
+                                        backgroundColor = null,
+                                        font = cellFont.FontName,
+                                        fontSize = cellFont.FontHeight,
+                                        foregroundColor = null,
+                                        isBold = cellFont.IsBold,
+                                        isItalic = cellFont.IsItalic,
+                                        isUnderline = cellFont.Underline != null ? true : false,
+                                        isStrikeout = cellFont.IsStrikeout,
+                                        verticalAlignment = cell.CellStyle.VerticalAlignment
+                                    },
+                                    
+
+                                };
+                                
+
+                                //var cellNodesList = new List<TreeNode>();
+
+                                //cellNodesList.Add(new TreeNode(nameof(cell.RowIndex) + " = " + cell.RowIndex));
+                                //cellNodesList.Add(new TreeNode(nameof(cell.ColumnIndex) + " = " + cell.ColumnIndex));
+
+                                //cellNodesList.Add(
+                                //    new TreeNode(nameof(cell.CellStyle.Alignment) + " = " + cell.CellStyle.Alignment));
+
+                                //cellNodesList.Add(new TreeNode(nameof(cell.IsMergedCell) + " = " + cell.IsMergedCell));
+
+                           
+                                //cellNodesList.Add(new TreeNode(nameof(cellFont.FontName) + " = " + cellFont.FontName));
+                                //cellNodesList.Add(new TreeNode(nameof(cellFont.Color) + " = " + cellFont.Color));
+                                //cellNodesList.Add(new TreeNode(nameof(cellFont.IsBold) + " = " + cellFont.IsBold));
+                                //cellNodesList.Add(new TreeNode(nameof(cellFont.IsItalic) + " = " + cellFont.IsItalic));
+                                //cellNodesList.Add(
+                                //    new TreeNode(nameof(cellFont.FontHeight) + " = " + cellFont.FontHeight));
+
 
                                 var formatCellValue = formatter.FormatCellValue(cell);
-                                cellNodesList.Add(new TreeNode(
-                                    nameof(formatCellValue) + " = " + formatCellValue));
+                                //cellNodesList.Add(new TreeNode(
+                                //    nameof(formatCellValue) + " = " + formatCellValue));
+                                cellObject.displayContent = formatCellValue;
+                                string cellContent = null;
                                 if (cell.CellType == CellType.String)
                                 {
-                                    cellNodesList.Add(new TreeNode(
-                                        nameof(cell.RichStringCellValue) + " = " + cell.RichStringCellValue));
-                                    cellNodesList.Add(
-                                        new TreeNode(nameof(cell.StringCellValue) + " = " + cell.StringCellValue));
+                                    //cellNodesList.Add(new TreeNode(
+                                    //    nameof(cell.RichStringCellValue) + " = " + cell.RichStringCellValue));
+                                    //cellNodesList.Add(
+                                    //    new TreeNode(nameof(cell.StringCellValue) + " = " + cell.StringCellValue));
+
+                                    cellContent = cell.RichStringCellValue.ToString();
+                                    cellObject.cellType = Model.CellType.String;
                                 }
                                 else if (cell.CellType == CellType.Numeric)
                                 {
                                     var myCell = cell;
-                                    cellNodesList.Add(new TreeNode(
-                                        nameof(cell.NumericCellValue) + " = " + cell.NumericCellValue));
+                                    //cellNodesList.Add(new TreeNode(
+                                    //    nameof(cell.NumericCellValue) + " = " + cell.NumericCellValue));
+                                    cellContent = cell.NumericCellValue.ToString();
+                                    cellObject.cellType = Model.CellType.Numeric;
                                 }
                                 else if (cell.CellType == CellType.Formula)
                                 {
-                                    cellNodesList.Add(
-                                        new TreeNode(nameof(cell.CellFormula) + " = " + cell.CellFormula));
+                                    //cellNodesList.Add(
+                                    //    new TreeNode(nameof(cell.CellFormula) + " = " + cell.CellFormula));
+                                    cellContent = cell.CellFormula;
+                                    cellObject.cellType = Model.CellType.Formula;
                                 }
                                 else if (cell.CellType == CellType.Boolean)
                                 {
-                                    cellNodesList.Add(
-                                        new TreeNode(nameof(cell.BooleanCellValue) + " = " + cell.BooleanCellValue));
+                                    //cellNodesList.Add(
+                                    //    new TreeNode(nameof(cell.BooleanCellValue) + " = " + cell.BooleanCellValue));
+                                    cellContent = cell.BooleanCellValue.ToString();
+                                    cellObject.cellType = Model.CellType.Boolean;
                                 }
 
-                                cellNodesList.Add(new TreeNode(nameof(cell.CellComment) + " = " + cell.CellComment));
+                                cellObject.content = cellContent;
 
-                                var styleNode = new TreeNode("Cell : " + cell.ColumnIndex, cellNodesList.ToArray());
-                                _treeCellsList.Add(styleNode);
+                                //cellNodesList.Add(new TreeNode(nameof(cell.CellComment) + " = " + cell.CellComment));
+
+                                //var styleNode = new TreeNode("Cell : " + cell.ColumnIndex, cellNodesList.ToArray());
+                                //_treeCellsList.Add(styleNode);
+
+                                cellsList.Add(cellObject);
+
                             }
                         }
+
+                        var fileName = "C:\\Users\\FAURE\\Desktop\\export.txt";
+
+                        var json = JsonConvert.SerializeObject(cellsList, Formatting.Indented);
+                        File.WriteAllText(fileName, json);
 
                         var treeNodeRow = new TreeNode("Row " + rowNum, _treeCellsList.ToArray());
                         _treeRowsList.Add(treeNodeRow);
@@ -114,7 +172,7 @@ namespace TestExcelParser
                 }
 
                 // Ajout des feuilles à la listView
-                var treeNode = new TreeNode("Sheet " + s, _treeRowsList.ToArray());
+                var treeNode = new TreeNode("Sheet " + sheetIndex, _treeRowsList.ToArray());
                 treeView.Nodes.Add(treeNode);
             }
 
---
+++ Model/Cell.cs
@@ -0,0 +1,51 @@
+﻿using NPOI.SS.UserModel;
+
+namespace TestExcelParser.Model
+{
+    internal class Cell
+    {
+        public CellType cellType;
+        public int column;
+        public string content;
+        public string displayContent;
+        public int row;
+        public int sheet;
+        public Style style;
+    }
+
+    internal class Style
+    {
+        public HorizontalAlignment alignment;
+        public string backgroundColor;
+        public string font;
+        public double fontSize;
+        public string foregroundColor;
+        public bool isBold;
+        public bool isItalic;
+        public bool isStrikeout;
+        public bool isUnderline;
+        public VerticalAlignment verticalAlignment;
+    }
+
+    internal enum CellType
+    {
+        String,
+        Numeric,
+        Boolean,
+        Date,
+        Formula,
+        Blank
+    }
+
+    internal class Sheet
+    {
+        public File file;
+        public string name;
+        public string position;
+    }
+
+    internal class File
+    {
+        public string name;
+    }
+}
