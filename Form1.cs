69897c2291def2795779066bcb7b3c5c77e2e3fc
Alexandre FAURÉ <alexandre.faure@edisys.eu>
2020-03-06 12:00:01 +0100

    Ajout du bouton permettant l'ouverture d'un document Word

--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Windows.Forms;
+using NPOI.XWPF.UserModel;
 
 namespace TestExcelParser
 {
     public partial class Form1 : Form
     {
-        private static string filePath = "D:\\tests\\4573-DCE-DPGF ind B.xlsx";
+        private static string excelFilePath = "D:\\tests\\4573-DCE-DPGF ind B.xlsx";
+        private static string wordFilePath = "C:\\Users\\FAURE\\Documents\\die_4816176\\mail N2.docx";
 
         public Form1()
         {
@@ -15,18 +18,26 @@ namespace TestExcelParser
 
         private void button1_Click(object sender, EventArgs e)
         {
+            var path = GetFilePath(excelFilePath);
+
+            var parser = new ExcelParser();
+            var resultNodeCollection = parser.Parse(path);
+
+            FillListView(resultNodeCollection);
+        }
+
+        private static string GetFilePath(string filePath)
+        {
+            var path = filePath;
             if (string.IsNullOrEmpty(filePath))
             {
                 var fileManager = new FileManager();
                 fileManager.Open();
 
-                filePath = fileManager._filePath;
+                path = fileManager._filePath;
             }
 
-            var parser = new ExcelParser();
-            var resultNodeCollection = parser.Parse(filePath);
-
-            FillListView(resultNodeCollection);
+            return path;
         }
 
         private void FillListView(TreeNodeCollection resultNodeCollection)
@@ -43,6 +54,24 @@ namespace TestExcelParser
                 var treeNodeToAdd = new TreeNode(treeNode.Text, treeNodeList.ToArray());
 
                 treeView.Nodes.Add(treeNodeToAdd);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var path = GetFilePath(wordFilePath);
+
+            XWPFDocument document = null;
+            try
+            {
+                using (var file = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    document = new XWPFDocument(file);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new System.InvalidOperationException(ex.Message);
             }
         }
     }
